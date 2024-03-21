eae20eb8b849ebb65923e9a816bd0cb7df5eedeb
Sucharles <45457936+Sucharles@users.noreply.github.com>
2024-03-21 10:50:21 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "市场部AI小助手",
+  description: "五角场万达广场市场部专属AI小助手",
   viewport: {
     width: "device-width",
     initialScale: 1,
